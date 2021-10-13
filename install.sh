d18af7a15b85344e24d7e071f08da5c98fa218b3
Jonathyn Stiverson <jstiverson2002@gmail.com>
2021-10-13 16:37:17 +0000

    Update install.sh
    Add automated plugin install with Vundle. This moves the installation of vim
    plugins to the install script rather than when the user launched vim.

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 rm -f ~/.vimrc
 rm -rf ~/.vim
 
@@ -8,4 +7,8 @@ rm -rf ~/.vim
 cp -f ./.vimrc ~/.vimrc
 mkdir ~/.vim
 
+
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+
+vim +PluginInstall +qall
